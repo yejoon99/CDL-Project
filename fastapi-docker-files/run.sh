3932a199b94b0f27eccac43cb838eff9cec3de5e
Mateusz Ryczek <7564427+meowteusz@users.noreply.github.com>
2024-06-04 15:02:29 -0500

    Update run.sh

--- fastapi-docker-files/run.sh
+++ fastapi-docker-files/run.sh
@@ -1,6 +1,6 @@
 docker build -t my-fastapi-app .
 
-docker run -d -p 8000:8000 -v "~/Desktop/cdl_practicum/model and data/fastapi copy/log:/app/logs" --name fastapi-container my-fastapi-app
+docker run -d -p 8001:8001 -v "~/Desktop/cdl_practicum/model and data/fastapi copy/log:/app/logs" --name fastapi-container my-fastapi-app
 
 curl -X POST "http://localhost:8001/inference/" -H "Content-Type: application/json" -d '{"key": "value”}'
 
